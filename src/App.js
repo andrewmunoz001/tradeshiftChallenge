09dbf54e677e895bab2b500a7de9d42d1b7a3dc4
andrewmunoz001 <amuno032@ucr.edu>
2019-11-18 13:49:14 -0800

    Created Triangle Factory. Added basic error handling

--- src/App.js
+++ src/App.js
@@ -1,40 +1,70 @@
 import React, { useState } from "react";
-import { Button, Form } from "react-tradeshift-ui/";
+import { Button, Form, Note } from "react-tradeshift-ui/";
+
+import { TriangleFactory } from "./TriangleFactory";
 
 function App() {
-  const [state, setState] = useState({});
+  const [state, setState] = useState({ currentTriangle: null });
+
+  let myFactory = new TriangleFactory();
+
   function submitTriangle() {
-    console.log(state);
+    try {
+      let newTriangle = myFactory.createTriangle(
+        state.one,
+        state.two,
+        state.three
+      );
+      setState(prev => ({
+        ...prev,
+        currentTriangle: newTriangle
+      }));
+      console.log(newTriangle.getType());
+    } catch (error) {
+      window.ts.ui.Notification.error(error.toString());
+    }
   }
+
   function handleChange(event) {
     let { name, value } = event.target;
 
     setState(prev => ({
       ...prev,
-      [name]: value
+      [name]: parseInt(value)
     }));
   }
+
   return (
     <div>
+      {state.currentTriangle != null && (
+        <div data-ts="Note">
+          <i class="ts-icon-triangleup"></i>
+          <p>{state.currentTriangle.getType()} </p>
+        </div>
+      )}
       <Button className="ts-primary" onClick={submitTriangle}>
         <span>Create Triangle</span>
       </Button>
       <Form>
         <fieldset>
           <label>
-            <span>text</span>
+            <span>Side One</span>
             <input name="one" onChange={handleChange} type="text" required />
           </label>
         </fieldset>
+      </Form>
+      <Form>
         <fieldset>
           <label>
-            <span>text</span>
+            <span>Side Two</span>
             <input name="two" onChange={handleChange} type="text" required />
           </label>
         </fieldset>
+      </Form>
+      <Form>
         <fieldset>
           <label>
-            <span>text</span>
+            <span>Side Three</span>
             <input name="three" onChange={handleChange} type="text" required />
           </label>
         </fieldset>
---
+++ src/TriangleFactory.js
@@ -0,0 +1,61 @@
+function TriangleFactory() {
+  this.createTriangle = (sideOne, sideTwo, sideThree) => {
+    let uniqueSides = this.validateTriangle(sideOne, sideTwo, sideThree);
+    switch (uniqueSides) {
+      case 1:
+        return new EqualateralTriangle(sideOne);
+      case 2:
+        return new IsoscelesTriangle(sideOne, sideTwo, sideThree);
+      case 3:
+        return new ScaleneTriangle(sideOne, sideTwo, sideThree);
+      default:
+        throw "Invalid Triangle";
+    }
+  };
+
+  this.validateTriangle = (sideOne, sideTwo, sideThree) => {
+    if (!isNaN(sideOne) && !isNaN(sideTwo) && !isNaN(sideThree)) {
+      if (
+        sideOne + sideTwo > sideThree &&
+        sideOne + sideThree > sideTwo &&
+        sideTwo + sideThree > sideOne
+      ) {
+        let uniqueSides = {};
+        [sideOne, sideTwo, sideThree].forEach(side => {
+          uniqueSides[side] = uniqueSides[side] ? uniqueSides[side] + 1 : 1;
+        });
+        return Object.keys(uniqueSides).length;
+      } else {
+        throw "Invalid Triangle: Any Two sides must be greater length than Third side";
+      }
+    }
+    return -1;
+  };
+}
+function BaseTriangle(sideOne, sideTwo, sideThree) {
+  this.sideOne = sideOne;
+  this.sideTwo = sideTwo;
+  this.sideThree = sideThree;
+  this.getType = () => {};
+}
+
+function EqualateralTriangle(side) {
+  BaseTriangle.call(this, side, side, side);
+  this.getType = () => {
+    return "Equalateral";
+  };
+}
+function IsoscelesTriangle(sideOne, sideTwo, sideThree) {
+  BaseTriangle.call(this, sideOne, sideTwo, sideThree);
+  this.getType = () => {
+    return "Isosceles";
+  };
+}
+function ScaleneTriangle(sideOne, sideTwo, sideThree) {
+  BaseTriangle.call(this, sideOne, sideTwo, sideThree);
+  this.getType = () => {
+    return "Scalene";
+  };
+}
+
+export { TriangleFactory };
