e73edd123e3af398ee07a032f7eb6b2221d785ae
andrewmunoz001 <amuno032@ucr.edu>
2019-11-17 20:06:54 -0800

    Simple button with three text fields

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button, Form } from "react-tradeshift-ui/";
 
 function App() {
+  const [state, setState] = useState({});
+  function submitTriangle() {
+    console.log(state);
+  }
+  function handleChange(event) {
+    let { name, value } = event.target;
+
+    setState(prev => ({
+      ...prev,
+      [name]: value
+    }));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button className="ts-primary" onClick={submitTriangle}>
+        <span>Create Triangle</span>
+      </Button>
+      <Form>
+        <fieldset>
+          <label>
+            <span>text</span>
+            <input name="one" onChange={handleChange} type="text" required />
+          </label>
+        </fieldset>
+        <fieldset>
+          <label>
+            <span>text</span>
+            <input name="two" onChange={handleChange} type="text" required />
+          </label>
+        </fieldset>
+        <fieldset>
+          <label>
+            <span>text</span>
+            <input name="three" onChange={handleChange} type="text" required />
+          </label>
+        </fieldset>
+      </Form>
     </div>
   );
 }
